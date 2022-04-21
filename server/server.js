1294d6272c6e2c0779b1fc37f0e03a87d2f21bff
Cannon Wilson <CannonGWilson@gmail.com>
2022-04-21 11:24:35 -0500

    Added API route to test

--- server/server.js
+++ server/server.js
@@ -4,6 +4,9 @@ const app = express();
 const publicPath = path.join(__dirname, '..', 'build');
 const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
+app.get('/api/v1', (req, res) => {
+    res.send('reached the api')
+})
 app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
 });
